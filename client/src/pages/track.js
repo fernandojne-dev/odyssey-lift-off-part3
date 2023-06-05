0f91a29e0772101dbf1d8b1350732fc1af242869
Fernando Nuñez <31429242+fernandojne20@users.noreply.github.com>
2023-06-04 21:41:16 -0500

    Chapter: 8 The use query hook with variables (#6)

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { gql, useQuery } from '@apollo/client'
 import { Layout, QueryResult } from '../components'
 import { useParams } from 'react-router-dom'
+import TrackDetail from '../components/track-detail'
 
 const GET_TRACK = gql`
   query GetTracks($trackId: ID!) {
@@ -27,8 +28,16 @@ const GET_TRACK = gql`
   }
 `
 const Track = () => {
+
   const { trackId }= useParams()
-  return <Layout></Layout>
+  const {loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId }
+  })
+  return (<Layout>
+    <QueryResult error={error} loading={loading} data={data}>
+      <TrackDetail track={data?.track} />
+    </QueryResult>
+  </Layout>)
 }
 
 export default Track
