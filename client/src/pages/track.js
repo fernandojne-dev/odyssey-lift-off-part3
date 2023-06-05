3d5f60b64b97b4861a672462da31e1299c4c7662
Fernando Nuñez <31429242+fernandojne20@users.noreply.github.com>
2023-06-04 21:25:45 -0500

    Chapter 7: Build the track page (#5)

---
+++ client/src/pages/track.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client'
+import { Layout, QueryResult } from '../components'
+import { useParams } from 'react-router-dom'
+
+const GET_TRACK = gql`
+  query GetTracks($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`
+const Track = () => {
+  const { trackId }= useParams()
+  return <Layout></Layout>
+}
+
+export default Track
